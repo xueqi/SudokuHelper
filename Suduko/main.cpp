657f918a2a3924b258daf9368a5357463e87f4b7
Xueqi Liu <xueqi.liu@gmail.com>
2016-09-14 16:25:49 -0400

    testSquare added

--- Suduko/main.cpp
+++ Suduko/main.cpp
@@ -6,10 +6,19 @@
 //  Copyright © 2016 Xueqi Liu. All rights reserved.
 //
 
-#include <iostream>
-
+#include "tools.hpp"
+#include "square.hpp"
+void testSquare();
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    banner();
+    testSquare();
+    bye();
     return 0;
+}
+
+void testSquare() {
+    cout << "construct A valid Fixed Square" << endl;
+    Square sq1('1');
+    cout << "sq1: " << sq1 << endl;
+    
 }
