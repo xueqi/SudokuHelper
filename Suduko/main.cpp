57f648585177a93daa3dc898e4d0e1a3ed2eb792
Xueqi Liu <xueqi.liu@gmail.com>
2016-09-14 15:01:27 -0400

    Initial Commit

---
+++ Suduko/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Suduko
+//
+//  Created by Xueqi Liu on 9/14/16.
+//  Copyright © 2016 Xueqi Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
